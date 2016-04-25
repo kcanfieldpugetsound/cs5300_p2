45de7f4b46477e4a19af8861aedd84a6f7795f42
Jerome <jf446@cornell.edu>
2016-04-25 15:45:39 -0400

    testFileReader, probably will not need in future

---
+++ TestFileReader.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.File;
+
+
+public class TestFileReader{
+	public static void main(String [] args){
+		
+
+		try{
+			Scanner sc = new Scanner(new File("edges.txt"));
+			String line = sc.nextLine();
+
+			String[] stringArray = line.split("\\s+");
+
+			for (int i = 0; i < stringArray.length; i++)
+				System.out.println("String at " + i + ": " + stringArray[i]);
+		}
+		catch(Exception e){
+			System.err.println("caught exception");
+		}
+		
+
+
+	}
+}
