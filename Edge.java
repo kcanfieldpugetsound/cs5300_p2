1f6e95593e185e23c4715854185196b7a9399c18
kcanfieldpugetsound <kcanfield@mac.com>
2016-04-25 15:55:47 -0400

    adds input file reader and Edge class

---
+++ Edge.java
@@ -0,0 +1,11 @@
+public class Edge{
+		
+	private int source;
+	private int destination;
+
+	public Edge(int src, int dest)
+	{
+		this.source = src;
+		this.destination = dest;
+	}
+}
---
+++ InputReader.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.util.*;
+
+
+class FileReader
+{
+
+// compute filter parameters for netid jf446 - Jerome Francis
+double fromNetID = 0.644; // 644 is 446 reversed
+double rejectMin = 0.9 * fromNetID;
+double rejectLimit = rejectMin + 0.01;
+
+
+// assume 0.0 <= rejectMin < rejectLimit <= 1.0
+boolean selectInputLine(double x) {
+	return ( ((x >= rejectMin) && (x < rejectLimit)) ? false : true );
+}
+
+// get the input file and filter the edges based on netID digits
+public static Set<Edge> makeEdgeSet()
+{
+	Set<Edge> edgeSet = new HashSet<Edge>();
+
+	try{
+		File edgesFile = new File("edges.txt");
+		Scanner sc = new Scanner(edgesFile);
+
+		while(sc.hasNextLine())
+		{
+			String line = sc.nextLine();
+			String[] array = line.split("\\s+");
+			Double filterNum = Double.valueOf(array[array.length-1]);
+			if(selectInputLine(filterNum))//for the lines we are keeping
+			{//add them to the edge set
+				int src = array[1];
+				int dest = array[2];
+				Edge newEdge = new Edge(src,dest);
+				edgeSet.add(newEdge);
+			}
+		}
+
+	}
+	catch(Exception e)
+	{
+		System.err.println("Error reading file");
+	}
+return edgeSet;
+}
+
+}
