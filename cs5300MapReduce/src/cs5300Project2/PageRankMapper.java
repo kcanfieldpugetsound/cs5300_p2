0608a90052b76a5e3b8e2ddf6a5a7ae34fecdb4b
jeromeFrancis <jf446@cornell.edu>
2016-04-27 18:40:40 -0400

    added naive mapper and reducer

--- cs5300MapReduce/src/cs5300Project2/PageRankMapper.java
+++ cs5300MapReduce/src/cs5300Project2/PageRankMapper.java
@@ -1,15 +1,46 @@
 package cs5300Project2;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class PageRankMapper extends Mapper<Text, Text, Text, Text> {
+	
+	private Text outKey = new Text(), outValue = new Text();
 
 	public void map(Text ikey, Text ivalue, Context context) throws IOException, InterruptedException {
-
+		
+		String[] nodeData = ivalue.toString().split("#");
+		
+		String graphData = "G" + ivalue.toString();
+		
+		outKey.set(nodeData[0]);
+		outValue.set(graphData);
+		
+		context.write(outKey, outValue);
+		
+		Integer node = Integer.parseInt(nodeData[0]);
+		
+		double pr = Double.parseDouble(nodeData[3]);
+		
+		ArrayList<String> adjNodes = new ArrayList<String>();
+		
+		if (nodeData[1].length() > 0){
+			String [] childNodes = nodeData[1].split(",");
+			
+			for (int i = 0; i < childNodes.length; i++){
+				outKey.set(childNodes[i]);
+				outValue.set("P" + (pr / childNodes.length));
+				context.write(outKey, outValue);
+			}
+				
+		}
+		
+		
+		
 	}
 
 }
--- cs5300MapReduce/src/cs5300Project2/PageRankReducer.java
+++ cs5300MapReduce/src/cs5300Project2/PageRankReducer.java
@@ -6,10 +6,50 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class PageRankReducer extends Reducer<Text, Text, Text, Text> {
+	
+	private static final double ALPHA = 0.85;
+	public static String CONF_NUM_NODES = "pagerank.numnodes";
+	private int numNodes; 
+	
+	@Override
+	protected void setup(Context context) throws IOException, InterruptedException{
+		numNodes = context.getConfiguration().getInt(CONF_NUM_NODES, 0);
+	}
+	
+	private Text outValue = new Text();
 
 	public void reduce(Text _key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-		// process values
+		
+		double newPageRank = 0.0;
+		
+		double oldPageRank = 0.0;
+		
+		String graph = "";
+		
 		for (Text val : values) {
+			String input = val.toString();
+			
+			if (input.substring(0, 1).equals("G")){
+				
+				String[] graphData = input.substring(1).split("#");
+				graph += graphData[0] + "#" + graphData[1] + "#" + graphData[3];
+				
+				
+				
+			}
+			else if (input.substring(0, 1).equals("P")){
+				newPageRank += Double.parseDouble(input.substring(1));
+			}
+			
+			newPageRank = (1.0 - ALPHA) / numNodes + ALPHA * newPageRank;
+			
+			graph += "#" + newPageRank;
+			
+			outValue.set(graph);
+			
+			context.write(_key, outValue);
+			
+			
 
 		}
 	}
